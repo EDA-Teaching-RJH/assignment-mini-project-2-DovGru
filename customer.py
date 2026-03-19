05eb47c1a41de6fce7510214cd58a3ca9752ab7f
Dovydas Gruzinskas <dg525@kent.ac.uk>
2026-03-19 13:44:54 +0000

    added customer class with validation

---
+++ customer.py
@@ -0,0 +1,19 @@
+from validator import is_valid_name, is_valid_phone, is_valid_plan
+
+class Customer:                      #constructor with class to create a new customer object 
+    def __init__(self, name, phone_number, plan):
+        if not is_valid_name(name):                
+            raise ValueError("Invalid name")       #validation, cant be empty 
+        
+        if not is_valid_phone(phone_number):
+            raise ValueError("Invalid phone number")     #follow UK number format validation using regex
+        
+        if not is_valid_plan(plan):
+            raise ValueError("Invalid plan")        #check its in the plans list 
+        
+        self.name = name                         #assign values to the object
+        self.phone_number = phone_number
+        self.plan = plan 
+
+    def __str__(self):
+        return f"Name: {self.name}, Phone: {self.phone_number}, Plan: {self.plan}"      #display info

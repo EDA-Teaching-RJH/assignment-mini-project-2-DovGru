604d2af7984b1a45ff6707101576ea2a25f57cb1
Dovydas Gruzinskas <dg525@kent.ac.uk>
2026-03-19 14:23:05 +0000

    finalised customerdatabase with csv save and load functionality

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 import csv
+from customer import Customer
 
 class CustomerDatabase:
     def __init__(self):
-        self.customer = []       #list to store customer details 
+        self.customers = []       #list to store customer details 
 
     def add_customer(self, customer):
-        self.customer.append(customer)    #add to the list 
+        self.customers.append(customer)    #add to the list 
 
     def save_to_csv(self, filename):                         #save customers to csv
         with open(filename, "w", newline="") as file:  
@@ -14,4 +15,18 @@ class CustomerDatabase:
             writer.writerow(["name", "phone_number", "plan"])          #write header
 
             for customer in self.customers:
-                writer.wrterow([customer.name, customer.phone_number, customer.plan])    #write each customer
+                writer.writerow([customer.name, customer.phone_number, customer.plan])    #write each customer
+    
+    def load_from_csv(self, filename):               #load customer from csv
+        self.customers = []               #clear list for no duplications
+
+        with open(filename, "r") as file:               #read mode
+            reader = csv.reader(file)                   #reads each row
+            next(reader)       #skips the header row
+
+            for row in reader:
+                name, phone_number, plan = row
+                customer = Customer(name, phone_number, plan)    #creates a customer object from file 
+                
+                self.customers.append(customer)       #add to list
+
